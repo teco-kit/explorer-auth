3f6c25610307c2af3f78c04849194a3867da705b
TobiasRoeddiger <usdkr@student.kit.edu>
2019-08-08 13:56:14 +0200

    added missing files

---
+++ src/controller/healthController.js
@@ -0,0 +1,14 @@
+
+/**
+ * log in user by refresh token and return jwt
+ */
+async function check(ctx) {
+
+	// TODO: add more logic here if needed
+
+	return ctx;
+}
+
+module.exports = {
+	check
+};
