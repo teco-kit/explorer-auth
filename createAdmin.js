0cf3dc546fe8cb5bb35adfb68616798719f46e91
Jenny Muenk <jennifer.muenk@gmail.com>
2020-06-11 15:53:15 +0200

    create admin script: add db connection, add node env

--- createAdmin.js
+++ createAdmin.js
@@ -1,25 +1,27 @@
 const mongoose     = require('mongoose');
 const Config       = require('config');
+const Model 			 = require('./src/models/userModel').model;
 
-const config = Config.get('server');
-const Model = require('./src/models/userModel').model;
-
-const email = process.env.npm_config_email;
-
-// connect to Mongo
-mongoose.connect(config.db, {useNewUrlParser: true});
+const email 			 = process.env.npm_config_email;
 
 // suppress deprecation warnings
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 
+const config = Config.get('server');
+mongoose.connect(config.db, {useNewUrlParser: true}).catch(e => console.log(e));
+
 async function makeUserAdmin(userEmail) {
 	try {
 		const user = await Model.findOne({email: userEmail});
 		if (user) {
-			user.role = 'admin';
-			await user.save();
-			console.log(`Successfully made '${email}' an admin user!`);
+			if (user.role === 'admin') {
+				console.log(`User with '${email}' already has admin rights!`);
+			} else {
+				user.role = 'admin';
+				await user.save();
+				console.log(`Successfully made '${email}' an admin user!`);
+			}
 		} else {
 			console.log(`User with email '${email}' not found!`);
 		}
@@ -29,10 +31,14 @@ async function makeUserAdmin(userEmail) {
 }
 
 (async () => {
-	if(email === undefined) {
-		console.error('Please provide a username with --email=\'email\'');
-	} else {
-		await makeUserAdmin(email);
+	try {
+		if (email === undefined) {
+			console.error('Please provide a username with --email=\'email\'');
+		} else {
+			await makeUserAdmin(email);
+		}
+		mongoose.disconnect().catch(e => console.log(e));
+	} catch (e) {
+		console.error(e);
 	}
-	mongoose.disconnect();
 })();
